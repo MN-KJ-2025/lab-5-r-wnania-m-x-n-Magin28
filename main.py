3ee499c4f6491f2a7a8d7a08c63ccedccf3a6e41
Magin28 <marcin.ostachowicz@gmail.com>
2025-11-05 09:25:17 +0100

    laby 05.11.2025 nieskończone

--- main.py
+++ main.py
@@ -24,28 +24,33 @@ def spare_matrix_Abt(m: int, n: int) -> tuple[np.ndarray, np.ndarray] | None:
             - Wektor b (m,).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
+    if not isinstance(m, int) or not isinstance(n, int) or n<1 or m<1:
+        return None
+
+    wek = np.linspace(0,1,num=m)
+    b = np.cos(4*wek)
+    A = np.vander(wek, n, increasing=True)
+
+    return (A ,b)
     pass
 
 
 def square_from_rectan(
     A: np.ndarray, b: np.ndarray
 ) -> tuple[np.ndarray, np.ndarray] | None:
-    """Funkcja przekształcająca układ równań z prostokątną macierzą współczynników
-    na kwadratowy układ równań.
-    A^T * A * x = A^T * b  ->  A_new * x = b_new
+    
+    if not isinstance(A, np.ndarray) or not isinstance(b, np.ndarray):
+        return None
+    if b.ndim != 1:
+        return None
+    if A.shape[0] != b.shape[0]:
+        return None
+    if A.ndim != 2:
+        return None
 
-    Args:
-        A (np.ndarray): Macierz A (m,n) zawierająca współczynniki równania.
-        b (np.ndarray): Wektor b (m,) zawierający współczynniki po prawej stronie równania.
-
-    Returns:
-        (tuple[np.ndarray, np.ndarray]):
-            - Macierz A_new o rozmiarze (n,n),
-            - Wektor b_new (n,).
-        Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
-    """
-    pass
-
+    A_new = np.matmul(A.T, A)
+    b_new = np.matmul(A.T, b)
+    return (A_new, b_new)
 
 def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray) -> float | None:
     """Funkcja obliczająca normę residuum dla równania postaci:
